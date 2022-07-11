6d018e765f2c94140a21fff57f7cf56056fa3b8d
Harshit Suthar <65647274+harshitsuthar77731@users.noreply.github.com>
2022-07-11 09:57:15 +0530

    Create Implement Stack Using Arrays.cpp

---
+++ Stack and Queue/Implement Stack Using Arrays.cpp
@@ -0,0 +1,20 @@
+//Function to push an integer into the stack.
+void MyStack :: push(int x)
+{
+    // Your Code
+    top++;
+    arr[top] = x;
+    
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    // Your Code       
+    if(top==-1)
+        return top;
+    int temp = arr[top];
+    top = top-1;
+    return temp;
+    
+}
