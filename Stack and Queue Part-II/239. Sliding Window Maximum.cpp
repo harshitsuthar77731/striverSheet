90f6ddab61beb406fd7fc16426248b805aefec9e
Harshit Suthar <65647274+harshitsuthar77731@users.noreply.github.com>
2022-07-19 16:06:12 +0530

    Create 239. Sliding Window Maximum.cpp

---
+++ Stack and Queue Part-II/239. Sliding Window Maximum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int i =0 ;
+        int j = 0;
+        vector<int>v;
+        int sum = 0;
+        deque<int>q;
+        while(j<nums.size()){
+            while(q.size()>0&&nums[j]>q.back())
+                q.pop_back();
+            q.push_back(nums[j]);
+            
+            if(j-i+1==k){
+                
+                v.push_back(q.front());
+                if(q.front()==nums[i])
+                    q.pop_front();
+                i++;
+            }
+            j++;
+        }
+        return v;
+    }
+};
