885bb99d656249f290760cfffed0f8d6fc41147d
Harshit Suthar <65647274+harshitsuthar77731@users.noreply.github.com>
2022-08-08 11:27:41 +0530

    Update 287. Find the Duplicate Number.cpp

--- Arrays part-II/287. Find the Duplicate Number.cpp
+++ Arrays part-II/287. Find the Duplicate Number.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(auto x:nums){
-            if(mp[x]>0)
-                return x;
-            mp[x]++;
+        int n = nums.size();
+        for(int i=0;i<n;){
+            if(nums[i]-1==i)
+                i++;
+            else{
+                if(nums[i]==nums[nums[i]-1]){
+                    return nums[i];
+                }
+                else{
+                    swap(nums[i],nums[nums[i]-1]);
+                }
+            }
         }
         return -1;
     }
