87db81e36b619fe2d1e9929b3bd2049f4e8f5242
Harshit Suthar <65647274+harshitsuthar77731@users.noreply.github.com>
2022-08-23 18:14:36 +0530

    Update 31. Next Permutation.cpp

--- Arrays/31. Next Permutation.cpp
+++ Arrays/31. Next Permutation.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        vector<int>v;
-        int pre = nums[nums.size()-1];
-        int ind = -1;
-        int temp;
         for(int i=nums.size()-2;i>=0;i--){
-            // cout<<nums[i]<<" "<<pre<<endl;
-            if(nums[i]<pre){
-                temp = nums[i];
-                sort(nums.begin()+i,nums.end());
-                ind = i;
-                if(nums[nums.size()-1]>=temp){
-                    for(int j = i ;j<nums.size();j++){
-                        
-                        if(nums[j]>temp){
-                            
-                            swap(nums[i],nums[j]);
-                            break;
-                        }
+            if(nums[i]<nums[i+1]){
+                sort(nums.begin()+i+1,nums.end());
+                for(int j = i+1;j<nums.size();j++){
+                    if(nums[i]<nums[j]){
+                        swap(nums[i],nums[j]);
+                        break;
                     }
-                    sort(nums.begin()+i+1,nums.end());
-                    break;
                 }
-                else 
-                    swap(nums[i+1],nums[i]);
-                
-                break;
+                sort(nums.begin()+i+1,nums.end());
+                return;
             }
-            pre = nums[i];
         }
-        
-        if(ind==-1)
-            reverse(nums.begin(),nums.end());
-       
+        sort(nums.begin(),nums.end());
     }
 };
