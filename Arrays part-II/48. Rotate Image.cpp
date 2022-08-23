d7a3769de582aaaa4065ecee061b8feecf61a614
Harshit Suthar <65647274+harshitsuthar77731@users.noreply.github.com>
2022-08-23 20:05:30 +0530

    Update 48. Rotate Image.cpp

--- Arrays part-II/48. Rotate Image.cpp
+++ Arrays part-II/48. Rotate Image.cpp
@@ -1,3 +1,26 @@
+// without extra space
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        //transpose
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        //swap
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                    swap(matrix[i][j],matrix[i][n-1-j]);
+            }
+        }
+    }
+};
+
+
+// with extra space
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
